5fa79e871b3e7ea38bbf09369e522256df2c2f79
sirSayed98 <mohamed.sayed8899@hotmail.com>
2021-04-27 23:06:51 +0200

    add congratulation message to confirm Email

--- mazad/components/ConfirmEmail/ConfirmEmailScreen.js
+++ mazad/components/ConfirmEmail/ConfirmEmailScreen.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Link from "next/link";
+import { useSelector } from "react-redux";
+import { useRouter } from "next/router";
 
+import style from "./ConfirmEmailScreen.module.css";
 const ConfirmEmailScreen = () => {
-    return (
-        <>
-            
-        </>
-    )
-}
+  const router = useRouter();
+  const userRegister = useSelector((state) => state.userRegister);
+  const { success } = userRegister;
 
-export default ConfirmEmailScreen
+  useEffect(() => {
+    if (!success) {
+      router.push("/");
+    }
+  }, [success]);
+  return (
+    <div className={`${style.card_container}`}>
+      <div className={`${style.card_body} col-sm-12 col-lg-6 col-xl-4`}>
+        <div className={`${style.card_header} mb-4 text-capitalize `}>
+          <h1 className="display-4">Congratulation</h1>
+          <h6 className="display-5">
+            Please Check Your Email to complete your regestration
+          </h6>
+        </div>
+        <div className={style.card_bottom}>
+          <div className="px-5">
+            <hr className={style.newLine} />
+          </div>
+          <button className="btn btn-lg master_button">
+            <Link href="/">GO HOME</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmEmailScreen;
--- mazad/pages/confirmEmail.js
+++ mazad/pages/confirmEmail.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Navber from "../components/Navbar/Navbar";
+import ConfirmEmailScreen from "../components/ConfirmEmail/ConfirmEmailScreen";
 const confirmEmail = () => {
   return (
     <>
       <Navber />
+      <ConfirmEmailScreen />
     </>
   );
 };
