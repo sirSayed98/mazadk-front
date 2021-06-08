3e88941be060668ae5d09d944e2482040fb591d6
sirSayed98 <mohamed.sayed8899@hotmail.com>
2021-06-08 15:16:24 +0200

    prevent unauthorized

--- mazad/pages/mazad/[id].js
+++ mazad/pages/mazad/[id].js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import Navbar from "../../components/Navbar/Navbar";
 import BiddingCard from "../../components/BiddingCard/BiddingCard";
 import Bidders from "../../components/Bidders/Bidders";
+import { useSelector } from "react-redux";
 
 const id = () => {
   const router = useRouter();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
+  useEffect(() => {
+    if (userInfo === null) {
+      router.push("/Login");
+    }
+  }, []);
   return (
     <>
       <Navbar />
       {router.query && <BiddingCard id={router.query.id} />}
       <div className="container">
-        <Bidders  />
+        <Bidders />
       </div>
     </>
   );
